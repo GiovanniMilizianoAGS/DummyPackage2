34dd913f252f0edf11fb7ce4d1c0c5366c33d8b5
Giovanni Miliziano <gmiliziano@playags.com>
2026-07-24 11:56:48 -0400

    Initial commit

---
+++ Packages/com.gvm.b1/Runtime/Scripts/componentdum.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class componentdum : MonoBehaviour
+{
+    public int data1;
+    public string data2;
+    public float data3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
